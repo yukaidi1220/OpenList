9bbf401d0b7b7862a59911902b8c243d0b7e5e15
yukaidi <yukaidi@duck.com>
2026-06-07 02:50:35 +0800

    feat(onedrive): change LinkExpireSeconds default from 0 to 1200

--- drivers/onedrive/meta.go
+++ drivers/onedrive/meta.go
@@ -23,7 +23,7 @@ type Addition struct {
 	DisableDiskUsage   bool   `json:"disable_disk_usage" default:"false"`
 	EnableDirectUpload bool   `json:"enable_direct_upload" default:"false" help:"Enable direct upload from client to OneDrive"`
 	CreateShareLink    bool   `json:"create_share_link" default:"false" help:"Create share link for file download"`
-	LinkExpireSeconds  int64  `json:"link_expire_seconds" type:"number" default:"0" help:"Link cache duration in seconds, 0 means no cache"`
+	LinkExpireSeconds  int64  `json:"link_expire_seconds" type:"number" default:"1200" help:"Link cache duration in seconds, 0 means no cache"`
 }
 
 var config = driver.Config{
