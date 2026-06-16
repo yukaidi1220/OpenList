fe9513e2bf020cde834799178401d963f9890376
yukaidi <yukaidi@duck.com>
2026-06-17 02:39:04 +0800

    perf(139): 恢复分片上传大小为100MB

--- drivers/139/driver.go
+++ drivers/139/driver.go
@@ -613,8 +613,8 @@ func (d *Yun139) getPartSize(size int64) int64 {
 	// if size/utils.GB > 30 {
 	// 	return 512 * utils.MB
 	// }
-	// return 100 * utils.MB
-	return 10 * utils.MB
+	return 100 * utils.MB
+	// return 10 * utils.MB
 }
 
 func (d *Yun139) Put(ctx context.Context, dstDir model.Obj, stream model.FileStreamer, up driver.UpdateProgress) error {
